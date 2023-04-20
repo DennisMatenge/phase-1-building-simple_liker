ad87fc08804d86e4cbe585a7d008eff9dbf562b6
DennisMatenge <mateshdennis@gmail.com>
2023-04-20 22:00:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// Defining text characters for the empty and full hearts for you to use later.
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+
+// Your JavaScript code goes here!
+const likeGlyph = document.querySelector('.like-glyph');
+const modal = document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
+
+modal.classList.add('hidden');
+
+likeGlyph.addEventListener('click', () => {
+  mimicServerCall()
+    .then(() => {
+      likeGlyph.innerHTML = '❤';
+      likeGlyph.classList.add('activated-heart');
+    })
+    .catch(error => {
+      modalMessage.innerText = error;
+      modal.classList.remove('hidden');
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
+});
+
+
+
+
+
+
+//------------------------------------------------------------------------------
+// Don't change the code below: this function mocks the server response
+//------------------------------------------------------------------------------
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
